75cf375c599a72fa2b98588b12142b9ef5a9e460
Rizwan <rizwanmushtaq15@gmail.com>
2023-08-06 14:46:35 +0200

    using refresh tokens in auth process

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -4,6 +4,7 @@ import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
 import { AuthenticationService } from './authentication.service';
 import { AuthType } from '../enums/auth-type.enum';
 import { Auth } from '../decorators/auth.decorator';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Auth(AuthType.None)
 @Controller('authentication')
@@ -19,5 +20,11 @@ export class AuthenticationController {
   @Post('sign-in')
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
+  }
+
+  @HttpCode(HttpStatus.OK) // changed since the default is 201
+  @Post('refresh-tokens')
+  refreshTokens(@Body() refreshTokenDto: RefreshTokenDto) {
+    return this.authService.refreshTokens(refreshTokenDto);
   }
 }
